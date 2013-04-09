d57e8f53d5a12ff657e09ef763bede46b4b27554
Chris Lee <finges@gmail.com>
2013-04-09 15:30:13 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nwstracker::Application.config.secret_token = '2a61218233f7a9dc3a171562419394626fba55763f60e0dfb79244e12e4eb52b5e51eba83000ddb0f5edd4a1516bdeac8e15952f36ebbcb6e08d9f0a82217bba'
